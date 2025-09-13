f1a125e91ca1df387c6f13b14a5dd45104555326
morsalinamunmun <morsalinamunmun@gmail.com>
2025-09-14 01:56:46 +0600

    frontend link setup

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,9 @@ import { globalErrorHandler } from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/NotFound";
 
 const app = express();
+app.use(cors({origin: ['http://localhost:8080', 'https://parcel-stride.vercel.app']}));
 app.use(express.json());
-app.use(cors())
+// app.use(cors())
 
 app.use("/api/v1", router);
 
