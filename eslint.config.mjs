92dae4cc177ea3eca4995e431d78974ddf64cfdd
morsalinamunmun <morsalinamunmun@gmail.com>
2026-03-31 16:45:21 +0600

    Refactor ESLint configuration, update server initialization, and correct Vercel routing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,35 @@
-// @ts-check
+// // @ts-check
 
+// import eslint from '@eslint/js';
+// import tseslint from 'typescript-eslint';
+
+// export default tseslint.config(
+//   eslint.configs.recommended,
+// //   tseslint.configs.recommended,
+// tseslint.configs.strict,
+//   tseslint.configs.stylistic,
+//   {
+//     rules:{
+//         "no-console": "warn",
+//     }
+//   }
+// );
+
+// @ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-//   tseslint.configs.recommended,
-tseslint.configs.strict,
-  tseslint.configs.stylistic,
-  {
-    rules:{
-        "no-console": "warn",
-    }
-  }
-);
+export default tseslint.config({
+  ignores: ["dist", "node_modules"],
+
+  extends: [
+    eslint.configs.recommended,
+    // tseslint.configs.recommended, // optional
+    tseslint.configs.strict,
+    tseslint.configs.stylistic,
+  ],
+
+  rules: {
+    "no-console": "warn",
+  },
+});
